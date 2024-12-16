a52562acbf2eede41da3f336d7130b29ae35e436
Ben Sheldon <bsheldon02@gmail.com>
2024-12-15 18:17:57 -0600

    Day 5 Part 2 Solution

--- day5.py
+++ day5.py
@@ -9,6 +9,24 @@ def isValid(line):
         return False
   return True
 
+def reorder(line):
+  i = 1
+  while i < len(line):
+    j = i - 1
+    while j >= 0:
+      if line[j] in graph[line[i]]:
+        break
+      j -= 1
+    if j == -1:
+      i += 1
+      continue
+    temp = line[i]
+    for k in range(i, j, -1):
+      line[k] = line[k - 1]
+    line[j] = temp
+    i = 1
+  return line[len(line) // 2]
+
 with open('day5.txt') as f:
   for line in f:
     line = line.replace("\n", "")
@@ -25,5 +43,12 @@ with open('day5-2.txt') as f:
 
 print("Part 1 Answer:", res)
 
+res2 = 0
+with open('day5-2.txt') as f:
+  for line in f:
+    line = line.replace("\n", "")
+    line = line.split(",")
+    if not isValid(line):
+      res2 += int(reorder(line))
 
-
+print("Part 2 Answer:", res2)
